07621e426be1f9a0d263292382251229e684f704
samdutton <samdutton@gmail.com>
2014-03-19 16:48:27 +0000

    gum tweak

--- samples/web/js/lib/ga.js
+++ samples/web/js/lib/ga.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-48530561-1', 'github.com');
+ga('create', 'UA-48530561-1', 'webrtc-samples');
 ga('send', 'pageview');
