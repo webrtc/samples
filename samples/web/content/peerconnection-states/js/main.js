e1a6fbef91bd1e8abfa70b021fec7805033d5265
Vikas Marwaha <vikasmarwaha@google.com>
2014-05-27 17:19:22 -0700

    Added License to webrtc samples.

--- samples/web/content/peerconnection-states/js/main.js
+++ samples/web/content/peerconnection-states/js/main.js
@@ -1,3 +1,12 @@
+/*
+ *  Copyright (c) 2014 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
 var video1 = document.querySelector('video#video1');
 var video2 = document.querySelector('video#video2');
 
--- samples/web/content/peerconnection/js/main.js
+++ samples/web/content/peerconnection/js/main.js
@@ -1,3 +1,12 @@
+/*
+ *  Copyright (c) 2014 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
 var startButton = document.getElementById('startButton');
 var callButton = document.getElementById('callButton');
 var hangupButton = document.getElementById('hangupButton');
