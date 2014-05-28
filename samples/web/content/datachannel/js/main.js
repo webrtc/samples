b6736da92cbb2d7d2c413ab0ba5d2ca6802ce70b
Vikas Marwaha <vikasmarwaha@google.com>
2014-05-28 11:32:51 -0700

    Added License to webrtc samples.
    edited the description that goes on top of source.

--- samples/web/content/datachannel/js/main.js
+++ samples/web/content/datachannel/js/main.js
@@ -3,9 +3,7 @@
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
- *  tree. An additional intellectual property rights grant can be found
- *  in the file PATENTS.  All contributing project authors may
- *  be found in the AUTHORS file in the root of the source tree.
+ *  tree.
  */
 var localConnection, remotePeerConnection, sendChannel, receiveChannel, pcConstraint, dataConstraint;
 var dataChannelSend = document.querySelector('textarea#dataChannelSend');
--- samples/web/js/adapter.js
+++ samples/web/js/adapter.js
@@ -3,9 +3,7 @@
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
- *  tree. An additional intellectual property rights grant can be found
- *  in the file PATENTS.  All contributing project authors may
- *  be found in the AUTHORS file in the root of the source tree.
+ *  tree.
  */
 var RTCPeerConnection = null;
 var getUserMedia = null;
