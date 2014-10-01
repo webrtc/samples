1eec09a60aa9cef9727cf7a3986d2fe1064cab12
Miguel Casas-Sanchez <mcasas@chromium.org>
2014-10-01 17:49:49 +0200

    gUM success/fail retrigger next gUM. Made some resolutions mandatory.

--- samples/web/content/testrtc/js/camresolutionstest.js
+++ samples/web/content/testrtc/js/camresolutionstest.js
@@ -11,40 +11,41 @@
 
 'use strict';
 
+/* This test tries calling getUserMedia() with a resolution out of a list below.
+ * Each gUM() call triggers a success or a fail callback, we report ok/nok and
+ * schedule another gUM() with the next resolution until the list is exhausted.
+ * Some resolutions are mandatory and make the test fail if not supported. 
+ *
+ * In generic cameras using Chrome rescaler, all resolutions should be supported
+ * up to a given one and none beyond there. Special cameras, such as digitizers,
+ * might support only one resolution.
+ */
+
 var CamResolutionsTest = {};
 CamResolutionsTest.index = 0;
 CamResolutionsTest.supported = 0;
 CamResolutionsTest.unsupported = 0;
-CamResolutionsTest.resolutions = [ [ 160, 120],
-                                   [ 320, 180],
-                                   [ 320, 240],
-                                   [ 640, 360],
-                                   [ 640, 480],
-                                   [1024, 576],
-                                   [1280, 720],
-                                   [1280, 768],
-                                   [1280, 800],
-                                   [1920,1080],
-                                   [3840,2160],
-                                   [4096,2160] ];
+// Each resolution has width, height and mandatory fields.
+CamResolutionsTest.resolutions = [ [ 160, 120 , false],
+                                   [ 320, 180 , false],
+                                   [ 320, 240 ,  true],  // QVGA
+                                   [ 640, 360 , false],
+                                   [ 640, 480 ,  true],  // VGA
+                                   [ 768, 576 , false],  // PAL
+                                   [1024, 576 , false],
+                                   [1280, 720 ,  true],  // HD
+                                   [1280, 768 , false],
+                                   [1280, 800 , false],
+                                   [1920,1080 , false],  // Full HD
+                                   [1920,1200 , false],
+                                   [3840,2160 , false],  // 4K
+                                   [4096,2160 , false] ];
+CamResolutionsTest.mandatory_yet_unsupported_resolutions = 0;
+CamResolutionsTest.numResolutions = CamResolutionsTest.resolutions.length;
 
 CamResolutionsTest.camResolutionsTest = function () {
-  var constraintsLength = CamResolutionsTest.resolutions.length;
-  trace('Checking ' + constraintsLength + ' constraint sets');
-
-  for (var i = 0; i < constraintsLength; ++i) {
-    var theResolution = CamResolutionsTest.resolutions[i];
-    try {
-      getUserMedia({ audio: false, video: { mandatory: {
-          minWidth:  theResolution[0], minHeight: theResolution[1],
-          maxWidth:  theResolution[0], maxHeight: theResolution[1] } } },
-          CamResolutionsTest.successFunc,
-          CamResolutionsTest.failFunc);
-    } catch (e) {
-      reportFatal('GetUserMedia failed.');
-    }
-  }
-  CamResolutionsTest.waitForTestToFinish();
+  trace('Checking ' + CamResolutionsTest.numResolutions + ' constraint sets');
+  CamResolutionsTest.TriggerGetUserMedia(CamResolutionsTest.resolutions[0]);
 }
 
 addTestSuite('CamResolutionsTest', CamResolutionsTest.camResolutionsTest);
@@ -56,30 +57,51 @@ CamResolutionsTest.successFunc = function(stream) {
   reportMessage('[   INFO ]', 'Supported resolution: (' +
             theResolution[0] + 'x' + theResolution[1] + ')');
   stream.stop();
+  CamResolutionsTest.finishTestOrRetrigger();
 }
 
 CamResolutionsTest.failFunc = function(error) {
   CamResolutionsTest.unsupported++;
   var theResolution =
       CamResolutionsTest.resolutions[CamResolutionsTest.index++];
-  reportMessage('[   INFO ]', 'Resolution NOT supported: (' +
-            theResolution[0] + 'x' + theResolution[1] + ')');
+  if (theResolution[2]) {
+    CamResolutionsTest.mandatory_yet_unsupported_resolutions++;
+    reportError('Camera does not support a mandatory resolution, (' +
+                theResolution[0] + 'x' + theResolution[1] + ')');
+  } else {
+    reportMessage('[   INFO ]', 'Resolution NOT supported: (' +
+                  theResolution[0] + 'x' + theResolution[1] + ')');
+  }
+  CamResolutionsTest.finishTestOrRetrigger();
 }
 
-CamResolutionsTest.waitForTestToFinish = function() {
-  var constraintsLength = CamResolutionsTest.resolutions.length;
-  if (CamResolutionsTest.index == constraintsLength) {
-    if (CamResolutionsTest.supported) {
-        reportSuccess(CamResolutionsTest.supported + '/' + constraintsLength +
+CamResolutionsTest.finishTestOrRetrigger = function() {
+  var numResolutions = CamResolutionsTest.numResolutions;
+  if (CamResolutionsTest.index == numResolutions) {
+    if (CamResolutionsTest.mandatory_yet_unsupported_resolutions == 0) {
+      if (CamResolutionsTest.supported) {
+        reportSuccess(CamResolutionsTest.supported + '/' + numResolutions +
                       ' resolutions supported.');
-    } else {
+      } else {
         reportError('No camera resolutions supported, most likely the camera' +
                     ' is not accessible or dead.');
+      }
     }
     testSuiteFinished();
   } else {
-    trace( 'Waiting for test to finish, ' + CamResolutionsTest.index + ' ?= ' +
-           (CamResolutionsTest.resolutions.length));
-    setTimeout('CamResolutionsTest.waitForTestToFinish()', 1000);
+    CamResolutionsTest.TriggerGetUserMedia(
+        CamResolutionsTest.resolutions[CamResolutionsTest.index]);
+  }
+}
+
+CamResolutionsTest.TriggerGetUserMedia = function(resolution) {
+  try {
+    getUserMedia({ audio: false, video: { mandatory: {
+        minWidth:  resolution[0], minHeight: resolution[1],
+        maxWidth:  resolution[0], maxHeight: resolution[1] } } },
+        CamResolutionsTest.successFunc,
+        CamResolutionsTest.failFunc);
+  } catch (e) {
+    reportFatal('GetUserMedia failed.');
   }
 }
