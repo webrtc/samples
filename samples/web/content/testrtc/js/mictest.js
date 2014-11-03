a80ac7a464b80e5d4b80fb06f8af41aa95d44113
Christoffer Jansson <cjansss@gmail.com>
2014-11-03 13:42:58 +0100

    Added validation of the input volume, if its too low fail the test. Added testing of input channels to determine how many are active and display the volume.

--- samples/web/content/testrtc/js/mictest.js
+++ samples/web/content/testrtc/js/mictest.js
@@ -7,54 +7,112 @@
  */
 'use strict';
 
-addTest('Microphone', 'Audio capture', micTest);
+addTest('Microphone', 'Audio capture', function() {
+  var test = new MicTest();
+  test.run();
+});
 
-function micTest() {
-  doGetUserMedia({audio:true}, function(stream) {
-    if (checkTracks(stream)) {
-      checkAudioStart(stream);
+function MicTest() {
+  this.inputChannels = 6;
+  this.outputChannels = 1;
+  this.lowVolumeThreshold = -60;
+  this.bufferSize = 4096;
+  this.activeChannels = [];
+}
+
+MicTest.prototype = {
+  run: function() {
+    doGetUserMedia({audio:true}, this.gotStream.bind(this));
+  },
+
+  gotStream: function(stream) {
+    if (!this.checkAudioTracks(stream)) {
+      testFinished();
+      return;
     }
-  });
-}
+    this.createAudioBuffer(stream);
+  },
 
-function checkTracks(stream) {
-  reportSuccess('getUserMedia succeeded.');
-  var tracks = stream.getAudioTracks();
-  if (tracks.length < 1) {
-    return reportFatal('No audio track in returned stream.');
-  }
-  var audioTrack = tracks[0];
-  reportSuccess('Audio track exists with label=' + audioTrack.label);
-  return true;
-}
+  checkAudioTracks: function(stream) {
+    this.stream = stream;
+    var audioTracks = stream.getAudioTracks();
+    if (audioTracks.length < 1) {
+      return reportFatal('No audio track in returned stream.');
+    }
+    reportSuccess('Audio track exists with label=' + audioTracks[0].label);
+    return true;
+  },
 
-// Analyze one buffer of audio.
-function checkAudioStart(stream) {
-  var processFunc = function(event) {
+  createAudioBuffer: function() {
+    this.audioSource = audioContext.createMediaStreamSource(this.stream);
+    this.scriptNode = audioContext.createScriptProcessor(this.bufferSize, 
+        this.inputChannels, this.outputChannels);
+    this.audioSource.connect(this.scriptNode);
+    this.scriptNode.connect(audioContext.destination);
+    this.scriptNode.onaudioprocess = this.processAudio.bind(this);
+  },
+
+  processAudio: function(event) {
     var inputBuffer = event.inputBuffer;
-    source.disconnect(scriptNode); 
-    scriptNode.disconnect(audioContext.destination);
-    stream.getAudioTracks()[0].stop();
-    checkAudioFinish(inputBuffer);
-  };
+    this.inputBuffer = inputBuffer;
+    this.stream.stop();
+    this.audioSource.disconnect(this.scriptNode);
+    this.scriptNode.disconnect(audioContext.destination);
+    // Start analazying the audio buffer.
+    reportInfo('Audio input sample rate=' + inputBuffer.sampleRate);
+    this.testNumberOfActiveChannels();
+    testFinished();
+  },
 
-  var source = audioContext.createMediaStreamSource(stream);
-  var scriptNode = audioContext.createScriptProcessor(4096, 1, 1);
-  scriptNode.onaudioprocess = processFunc;
-  source.connect(scriptNode);
-  scriptNode.connect(audioContext.destination);
-}
+  testNumberOfActiveChannels: function() {
+    var numberOfChannels = this.inputBuffer.numberOfChannels;
+    for (var channel = 0; channel < numberOfChannels; channel++) {
+      var numberOfZeroSamples = 0;
+      for (var sample = 0; sample < this.inputBuffer.length; sample++) {
+        if (this.inputBuffer.getChannelData(channel)[sample] === 0) {
+          numberOfZeroSamples++;
+        }
+      }
+      if (numberOfZeroSamples !== this.bufferSize) {
+        this.activeChannels[channel] = numberOfZeroSamples;
+        this.testInputVolume(channel);
+      }        
+    }
+    if (this.activeChannels.length === 0) {
+      reportFatal('No active input channels detected.');
+    } else {
+      reportSuccess('Audio input channels=' + this.activeChannels.length);
+    }
+    // If two channel input compare zero data samples to determine if it's mono.
+    if (this.activeChannels.length === 2) {
+      if (this.activeChannels[0][0] === this.activeChannels[1][0]) {
+        reportInfo('Mono stream detected.');
+      }
+    }
+  },
 
-function checkAudioFinish(buffer) {
-  reportSuccess('Audio num channels=' + buffer.numberOfChannels);
-  reportSuccess('Audio sample rate=' + buffer.sampleRate);
-  var data = buffer.getChannelData(0);
-  var sum = 0;
-  for (var sample = 0; sample < buffer.length; ++sample) {
-    sum += Math.abs(data[sample]);
+  testInputVolume: function(channel) {
+    var data = this.inputBuffer.getChannelData(channel);
+    var sum = 0;
+    for (var sample = 0; sample < this.inputBuffer.length; ++sample) {
+      sum += Math.abs(data[sample]);
+    }
+    var rms = Math.sqrt(sum / this.inputBuffer.length);
+    var db = Math.round(20 * Math.log(rms) / Math.log(10));
+
+    // Check input audio level.
+    if (db < this.lowVolumeThreshold) {
+      if (db === -Infinity ) {
+        return reportFatal('Audio input level=' + db  + 'db' + '\nMicrophone ' +
+                           'is most likely muted or broken, please check if ' +
+                           'muted in the sound settings or physically on the ' +
+                           'device.');
+      }
+      reportFatal('Audio input level=' + db + ' db' + '\nMicrophone input ' +
+                  'level is low, increase input volume or move closer to the ' +
+                  'microphone');
+    } else {
+      reportSuccess('Audio power for channel ' + channel + '=' + db + ' db');
+    }
   }
-  var rms = Math.sqrt(sum / buffer.length);
-  var db = 20 * Math.log(rms) / Math.log(10);
-  reportSuccess('Audio power=' + db);
-  testFinished();
-}
+};
