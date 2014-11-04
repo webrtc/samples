3c2f121a0c6cdb4ce64ef5a8009b6ea7e588f566
Sam Dutton <dutton@google.com>
2014-11-04 10:49:45 +0000

    Replaced all of web/content with new files and structure

--- samples/web/content/apprtc/apprtc.py
+++ samples/web/content/apprtc/apprtc.py
@@ -52,9 +52,6 @@ def get_preferred_audio_receive_codec():
 def get_preferred_audio_send_codec(user_agent):
   # Empty string means no preference.
   preferred_audio_send_codec = ''
-  # Prefer to send ISAC on Chrome for Android.
-  if is_chrome_for_android(user_agent):
-    preferred_audio_send_codec = 'ISAC/16000'
   return preferred_audio_send_codec
 
 # HD is on by default for desktop Chrome, but not Android or Firefox (yet)
