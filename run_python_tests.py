b1a81e45fffdf5cf88a40589dfb91bebc3b3cc98
Patrik Höglund <phoglund@chromium.org>
2014-12-01 16:54:36 +0100

    Made webtest tests work, wrote first AppRTC server test.

--- run_python_tests.py
+++ run_python_tests.py
@@ -8,14 +8,16 @@ import unittest
 USAGE = """%prog SDK_PATH TEST_PATH
 Run unit tests for App Engine apps.
 
-SDK_PATH    Path to the SDK installation
-TEST_PATH   Path to package containing test modules"""
+SDK_PATH     Path to the SDK installation.
+TEST_PATH    Path to package containing test modules.
+WEBTEST_PATH Path to the webtest library."""
 
 
-def main(sdk_path, test_path):
+def main(sdk_path, test_path, webtest_path):
     sys.path.insert(0, sdk_path)
     import dev_appserver
     dev_appserver.fix_sys_path()
+    sys.path.append(webtest_path)
     suite = unittest.loader.TestLoader().discover(test_path,
                                                   pattern="*test.py")
     unittest.TextTestRunner(verbosity=2).run(suite)
@@ -24,10 +26,11 @@ def main(sdk_path, test_path):
 if __name__ == '__main__':
     parser = optparse.OptionParser(USAGE)
     options, args = parser.parse_args()
-    if len(args) != 2:
+    if len(args) != 3:
         print 'Error: Exactly 2 arguments required.'
         parser.print_help()
         sys.exit(1)
     SDK_PATH = args[0]
     TEST_PATH = args[1]
-    main(SDK_PATH, TEST_PATH)
+    WEBTEST_PATH = args[2]
+    main(SDK_PATH, TEST_PATH, WEBTEST_PATH)
--- run_python_tests.sh
+++ run_python_tests.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
+set -e
+
+function download {
+  local filename=$1
+  local url=$2
+
+  if [ -f $filename ]; then
+    # Only download if newer than the one we have.
+    curl -z $filename -sS $url -o $filename
+  else
+    curl $url -sS -o $filename
+  fi
+}
+
 VERSION_REGEX='[0-9]*\.[0-9]*\.[0-9]*'
 VERSION=$(curl -sS https://appengine.google.com/api/updatecheck | grep release | grep -o $VERSION_REGEX)
-FILE=google_appengine_$VERSION.zip
-URL=https://storage.googleapis.com/appengine-sdks/featured/$FILE
+GAE_SDK_FILE=google_appengine_$VERSION.zip
+GAE_SDK_URL=https://storage.googleapis.com/appengine-sdks/featured/$FILE
 
-if [ -f $FILE ]; then
-  # Only download if newer than the one we have.
-  curl -z $FILE -sS -O $URL
-else
-  curl -sS -O $URL
+download $GAE_SDK_FILE $GAE_SDK_URL
+unzip -quo $GAE_SDK_FILE
+
+WEBTEST_FILE=webtest-master.tar.gz
+WEBTEST_URL=https://nodeload.github.com/Pylons/webtest/tar.gz/master
+
+if [ ! -d 'webtest-master' ]; then
+  download $WEBTEST_FILE $WEBTEST_URL
+  tar xvf $WEBTEST_FILE
+
+  # At least on my box, we must have root to modify your system python.
+  # This package only needs to be installed once.
+  echo "Missing webtest; must run sudo to install."
+  cd webtest-master
+  sudo python setup.py install
+  cd ..
 fi
-# Only update changed files.
-unzip -quo $FILE
 
-python run_python_tests.py google_appengine/ samples/web/content/apprtc/
+python run_python_tests.py google_appengine/ samples/web/content/apprtc/ webtest-master/
