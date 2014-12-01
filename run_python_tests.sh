b78be57c7d7f78b48d29fa8dfda63e2d104d6a6f
Patrik Höglund <phoglund@chromium.org>
2014-12-01 16:54:36 +0100

    Made webtest tests work, wrote first AppRTC server test.

--- run_python_tests.sh
+++ run_python_tests.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
-FILE=google_appengine_$(curl -sS https://appengine.google.com/api/updatecheck | grep release | grep -o '[0-9\.]*').zip
-URL=https://storage.googleapis.com/appengine-sdks/featured/$FILE
+set -e
 
-if [ -f $FILE ]; then
-  # Only download if newer than the one we have.
-  curl -z $FILE -sS -O $URL
-else
-  curl -sS -O $URL
+function download {
+  local filename=$1
+  local url=$2
+
+  if [ -f $filename ]; then
+    # Only download if newer than the one we have.
+    curl -z $filename -sS $url -o $filename
+  else
+    curl $url -sS -o $filename
+  fi
+}
+
+GAE_SDK_FILE=google_appengine_$(curl -sS https://appengine.google.com/api/updatecheck | grep release | grep -o '[0-9\.]*').zip
+GAE_SDK_URL=https://storage.googleapis.com/appengine-sdks/featured/$GAE_SDK_FILE
+
+download $GAE_SDK_FILE $GAE_SDK_URL
+unzip -quo $GAE_SDK_FILE
+
+WEBTEST_FILE=webtest-master.tar.gz
+WEBTEST_URL=https://nodeload.github.com/Pylons/webtest/tar.gz/master
+
+if [ ! -d 'webtest-master' ]; then
+  download $WEBTEST_FILE $WEBTEST_URL
+  tar xvf $WEBTEST_FILE
+
+  # At least on my box, we must have root to modify your system python.
+  # This package only needs to be installed once.
+  echo "Missing webtest; must run sudo to install."
+  cd webtest-master
+  sudo python setup.py install
+  cd ..
 fi
-# Only update changed files.
-unzip -quo $FILE
 
-python run_python_tests.py google_appengine/ samples/web/content/apprtc/
+python run_python_tests.py google_appengine/ samples/web/content/apprtc/ webtest-master/
--- samples/web/content/apprtc/apprtc_test.py
+++ samples/web/content/apprtc/apprtc_test.py
@@ -1,6 +1,7 @@
 # Copyright 2014 Google Inc. All Rights Reserved.
 
 import unittest
+import webtest
 
 import apprtc
 from google.appengine.api import channel
@@ -8,7 +9,7 @@ from google.appengine.ext import db
 from google.appengine.ext import testbed
 
 
-class AppRtcTest(unittest.TestCase):
+class AppRtcUnitTest(unittest.TestCase):
 
   def setUp(self):
     # First, create an instance of the Testbed class.
@@ -24,6 +25,21 @@ class AppRtcTest(unittest.TestCase):
   def testGenerateRandomGeneratesStringOfRightLength(self):
     self.assertEqual(17, len(apprtc.generate_random(17)))
     self.assertEqual(23, len(apprtc.generate_random(23)))
+
+
+class AppRtcMainPageHandlerTest(unittest.TestCase):
+  def setUp(self):
+    self.test_app = webtest.TestApp(apprtc.app)
+
+  def makeGetRequest(self, path):
+    # PhantomJS uses WebKit, so Safari is closest to the thruth.
+    return self.test_app.get(path, headers={'User-Agent': 'Safari'})
+
+  def testConnectingWithoutRoomIdRedirectsToGeneratedRoom(self):
+    response = self.makeGetRequest('/')
+    self.assertEqual(response.status_int, 302)
+    redirect_location = response.headers['Location']
+    self.assertRegexpMatches(redirect_location, '^http://localhost/\?r=[\d]+$')
 
 if __name__ == '__main__':
   unittest.main()
