e26d7cdc584614521679a5e5f02c508058437909
Christoffer Jansson <jansson@chromium.org>
2014-12-02 15:21:28 +0100

    fixed according to comments

--- samples/web/content/testrtc/js/main.js
+++ samples/web/content/testrtc/js/main.js
@@ -33,67 +33,72 @@ window.addEventListener('polymer-ready', function() {
 function GumHandler() {
   // Element definitions.
   this.gumErrorMessage = document.getElementById('gum-error-message');
-
-  this.setupOverlayProp = function(id) {
-    // Setup overlay properties.
-    document.getElementById(id).backdrop = true;
-    document.getElementById(id).autoCloseDisabled = true;
-    return document.getElementById(id);
-  };
-  this.gumRequestOverlay = this.setupOverlayProp('gum-request-overlay');
-  this.gumErrorOverlay = this.setupOverlayProp('gum-error-overlay');
+  this.gumRequestOverlay = document.getElementById('gum-request-overlay');
+  this.gumErrorOverlay = document.getElementById('gum-error-overlay');
+  this.setupOverlayProp(this.gumRequestOverlay);
+  this.setupOverlayProp(this.gumErrorOverlay);
 }
 
 GumHandler.prototype = {
+  setupOverlayProp: function(element) {
+    // Setup overlay properties.
+    element.backdrop = true;
+    element.autoCloseDisabled = true;
+    return;
+  },
+
   probePermissions: function() {
-    this.timeout = 0;
-    startButton.setAttribute('disabled', null);
+    startButton.disabled = true;
     this.pollInitialGum();
   },
 
-  initialGum: function() {
-    doGetUserMedia({audio: true, video: true},
-        function(stream) {
-          stream.stop();
-          this.gumState = 'allowed';
-          if (this.gumRequestOverlay.opened) {
-            this.gumRequestOverlay.close();
-          } else if (this.gumErrorOverlay.opened) {
-            this.gumErrorOverlay.close();
-          }
-        }.bind(this),
-        function(error) {
-          this.gumState = 'denied';
-          this.gumErrorMessage.innerHTML = error.name;
-          if (!this.gumErrorOverlay.opened) {
-            this.gumErrorOverlay.open();
-          }
-          if (this.gumRequestOverlay.opened) {
-            this.gumRequestOverlay.close();
-          }
-        }.bind(this)
-      );
-    this.gumFired = true;
-  },
-
   pollInitialGum: function() {
+    // Default timeout, not 0 because DOM needs time to load in order for
+    // overlay's to get correct dimensions.
+    var timeout = 300;
     if (this.gumState === 'allowed') {
-      startButton.removeAttribute('disabled');
+      startButton.disabled = false;
       return;
     }
     if (typeof this.gumState === 'undefined' && !this.gumFired) {
-      this.timeout = 300;
+      // First time users will hit this, i.e. no permissions set.
       this.initialGum();
     } else if (this.gumState === 'denied') {
-      this.timeout = 1000;
+      timeout = 1000;
       this.initialGum();
     } else {
       if (!this.gumRequestOverlay.opened) {
         this.gumRequestOverlay.open();
       }
     }
-    setTimeout(this.pollInitialGum.bind(this), this.timeout);
+    setTimeout(this.pollInitialGum.bind(this), timeout);
   },
+
+  initialGum: function() {
+    doGetUserMedia({audio: true, video: true}, this.gotStream.bind(this),
+        this.gotError.bind(this));
+    this.gumFired = true;
+  },
+
+  gotStream: function() {
+    this.gumState = 'allowed';
+    if (this.gumRequestOverlay.opened) {
+      this.gumRequestOverlay.close();
+    } else if (this.gumErrorOverlay.opened) {
+      this.gumErrorOverlay.close();
+    }
+  },
+
+  gotError: function(error) {
+    this.gumState = 'denied';
+    this.gumErrorMessage.innerHTML = error.name;
+    if (!this.gumErrorOverlay.opened) {
+      this.gumErrorOverlay.open();
+    }
+    if (this.gumRequestOverlay.opened) {
+      this.gumRequestOverlay.close();
+    }
+  }
 };
 
 // A test suite is a composition of many tests.
