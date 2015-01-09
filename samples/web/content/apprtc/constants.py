5a287975f2b132c4b3eca63b8d5eadfd5fe85fd7
Jiayang Liu <jiayl@chromium.org>
2015-01-09 14:50:38 -0800

    Add a cron job for probing CEOD

---
+++ samples/web/content/apprtc/constants.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2.4
+#
+# Copyright 2014 Google Inc. All Rights Reserved.
+
+"""AppRTC Constants
+
+This module contains the constants used in AppRTC Python modules.
+"""
+
+LOOPBACK_CLIENT_ID = 'LOOPBACK_CLIENT_ID'
+
+TURN_BASE_URL = 'https://computeengineondemand.appspot.com'
+TURN_URL_TEMPLATE = '%s/turn?username=%s&key=%s'
+CEOD_KEY = '4080218913'
+
+WSS_HOST_PORT_PAIR = 'apprtc-ws.webrtc.org:443'
