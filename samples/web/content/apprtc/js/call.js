b55d3447f9926628e69500a9e887ca4d5d65b8df
Chuck Hays <github@chuckhays.net>
2015-01-23 14:23:19 -0800

    Display buttons after hang up..

--- samples/web/content/apprtc/js/call.js
+++ samples/web/content/apprtc/js/call.js
@@ -54,6 +54,10 @@ Call.prototype.start = function(roomId) {
   }
 };
 
+Call.prototype.restart = function() {
+  this.start(this.params_.previousRoomId);
+};
+
 Call.prototype.hangup = function() {
   this.startTime = null;
 
@@ -85,6 +89,7 @@ Call.prototype.hangup = function() {
   this.channel_.send(JSON.stringify({type: 'bye'}));
   this.channel_.close();
 
+  this.params_.previousRoomId = this.params_.roomId;
   this.params_.roomId = null;
   this.params_.clientId = null;
 };
