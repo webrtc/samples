4436f4192fd04c853b901e6cd699aebb254559d4
Donald E Curtis <decurtis@google.com>
2015-01-26 20:43:04 -0800

    Add Google API dependencies.

---
+++ samples/web/content/apprtc/third_party/oauth2client/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.2"
+
+GOOGLE_AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
+GOOGLE_REVOKE_URI = 'https://accounts.google.com/o/oauth2/revoke'
+GOOGLE_TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
