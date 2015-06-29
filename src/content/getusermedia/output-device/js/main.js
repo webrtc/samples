959ce31072376b62d6ff1c4c3675edf4616f2590
Sam Dutton <dutton@google.com>
2015-06-29 16:21:10 +0100

    Added output-device

---
+++ src/content/getusermedia/output-device/js/main.js
@@ -0,0 +1,70 @@
+/*
+ *  Copyright (c) 2014 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+'use strict';
+
+var videoElement = document.querySelector('video');
+var audioSelect = document.querySelector('#audio select');
+
+audioSelect.onchange = selectOutputDevice;
+
+var constraints = window.constraints = {
+  audio: true,
+  video: true
+};
+
+function getDevices() {
+  navigator.mediaDevices.enumerateDevices().
+    then(gotDevices).
+    catch(errorCallback);
+}
+
+function gotDevices(infos) {
+  for (var i = 0; i !== infos.length; ++i) {
+    var info = infos[i];
+    var option = document.createElement('option');
+    option.value = info.deviceId;
+    if (info.kind === 'audiooutput') {
+      option.text = info.label || 'Audio output ' + (audioSelect.length + 1);
+      audioSelect.appendChild(option);
+      console.log('Audio output device found: ', info);
+    } else {
+      console.log('Device found, not audio output: ', info);
+    }
+  }
+}
+
+function successCallback(stream) {
+  attachMediaStream(videoElement, stream);
+  console.log('Got stream with constraints:', constraints);
+  var audioTracks = stream.getAudioTracks();
+  console.log('Using audio input: ' + audioTracks[0].label);
+  var videoTracks = stream.getVideoTracks();
+  console.log('Using video input: ' + videoTracks[0].label);
+  stream.onended = function() {
+    console.log('Stream ended');
+  };
+  window.stream = stream; // make variable available to browser console
+}
+
+function errorCallback(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+function selectOutputDevice() {
+  videoElement.setSinkId(audioSelect.value);
+}
+
+navigator.getUserMedia(constraints, successCallback, errorCallback);
+
+if (!navigator.mediaDevices) {
+  alert('This browser does not support navigator.mediaDevices. ' +
+    'Cannot select output devices.');
+} else {
+  getDevices();
+}
