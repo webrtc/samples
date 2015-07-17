ba31f5404b177fd74764a4ecb50474f1aab27d85
Philipp Hancke <fippo@andyet.net>
2015-07-17 15:00:08 -0700

    restyle for local code conventions

--- src/content/getusermedia/gum/js/test.js
+++ src/content/getusermedia/gum/js/test.js
@@ -2,7 +2,6 @@
 // This is a basic test file for use with testling.
 // The test script language comes from tape.
 /* jshint node: true */
-/* global Promise */
 var test = require('tape');
 
 // https://code.google.com/p/selenium/wiki/WebDriverJs
@@ -26,41 +25,43 @@ var croptions = new chrome.Options()
     .addArguments('use-fake-device-for-media-stream')
     .addArguments('use-fake-ui-for-media-stream');
 
+test('video width and video height are set on GUM sample', function(t) {
+  // FIXME: use env[SELENIUM_BROWSER] instead?
+  var driver = new webdriver.Builder()
+      .forBrowser(process.env.BROWSER)
+      .setFirefoxOptions(ffoptions)
+      .setChromeOptions(croptions)
+      .build();
 
-test('video width and video height are set on GUM sample', function (t) {
-    // FIXME: use env[SELENIUM_BROWSER] instead?
-    var driver = new webdriver.Builder()
-        .forBrowser(process.env['BROWSER'])
-        .setFirefoxOptions(ffoptions)
-        .setChromeOptions(croptions)
-        .build();
-
-    driver.get('https://webrtc.github.io/samples/src/content/getusermedia/gum/')
-    .then(function () {
-        t.pass('page loaded');
-        return driver.findElement(webdriver.By.id('gum-local'));
-    })
-    .then(function (videoElement) {
-        t.pass('found video element');
-        var width = 0;
-        var height = 0;
-        return new webdriver.promise.Promise(function (resolve) {
-            videoElement.getAttribute('videoWidth').then(function (w) {
-                width = w;
-                t.pass('got videoWidth ' + w);
-                if (width && height) resolve([width, height]);
-            });
-            videoElement.getAttribute('videoHeight').then(function (h) {
-                height = h;
-                t.pass('got videoHeight' + h);
-                if (width && height) resolve([width, height]);
-            });
-        });
-
-    })
-    .then(function (dimensions) {
-        t.pass('got video dimensions ' + dimensions.join('x'));
-        driver.quit();
-        t.end();
+  driver.get('https://webrtc.github.io/samples/src/content/getusermedia/gum/')
+  .then(function() {
+    t.pass('page loaded');
+    return driver.findElement(webdriver.By.id('gum-local'));
+  })
+  .then(function(videoElement) {
+    t.pass('found video element');
+    var width = 0;
+    var height = 0;
+    return new webdriver.promise.Promise(function(resolve) {
+      videoElement.getAttribute('videoWidth').then(function(w) {
+        width = w;
+        t.pass('got videoWidth ' + w);
+        if (width && height) {
+          resolve([width, height]);
+        }
+      });
+      videoElement.getAttribute('videoHeight').then(function(h) {
+        height = h;
+        t.pass('got videoHeight ' + h);
+        if (width && height) {
+          resolve([width, height]);
+        }
+      });
     });
+  })
+  .then(function(dimensions) {
+    t.pass('got video dimensions ' + dimensions.join('x'));
+    driver.quit();
+    t.end();
+  });
 });
