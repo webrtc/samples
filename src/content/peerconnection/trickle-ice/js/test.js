24c75069dc9e1d2e93a1e9dc8ff8f8e3595e99b3
Philipp Hancke <fippo@andyet.net>
2015-07-22 12:53:22 -0700

    moar tests since i broke this once

--- src/content/peerconnection/trickle-ice/js/test.js
+++ src/content/peerconnection/trickle-ice/js/test.js
@@ -35,3 +35,95 @@ test('Candidate Gathering', function(t) {
     t.end();
   });
 });
+
+test('Loading server data', function(t) {
+  var driver = seleniumHelpers.buildDriver();
+
+  driver.get('file://' + process.cwd() +
+      '/src/content/peerconnection/trickle-ice/index.html')
+  .then(function() {
+    t.pass('page loaded');
+    return driver.findElement(webdriver.By.css('#servers>option'));
+  })
+  .then(function(element) {
+    return new webdriver.ActionSequence(driver).
+        doubleClick(element).perform();
+  })
+  .then(function() {
+    return driver.findElement(webdriver.By.id('url')).getAttribute('value');
+  })
+  .then(function(value) {
+    t.ok(value !== '', 'doubleclick loads server data');
+    driver.close();
+    driver.quit();
+    t.end();
+  })
+  .then(null, function(err) {
+    t.fail(err);
+    driver.close();
+    driver.quit();
+    t.end();
+  });
+});
+
+test('Adding a server', function(t) {
+  var driver = seleniumHelpers.buildDriver();
+
+  driver.get('file://' + process.cwd() +
+      '/src/content/peerconnection/trickle-ice/index.html')
+  .then(function() {
+    t.pass('page loaded');
+    return driver.findElement(webdriver.By.id('url'))
+        .sendKeys('stun:stun.l.google.com:19302');
+  })
+  .then(function() {
+    t.pass('url input worked');
+    return driver.findElement(webdriver.By.id('add')).click();
+  })
+  .then(function() {
+    return driver.findElement(webdriver.By.css('#servers'))
+        .getAttribute('length');
+  })
+  .then(function(length) {
+    t.ok(length === '2', 'server added');
+    driver.close();
+    driver.quit();
+    t.end();
+  })
+  .then(null, function(err) {
+    t.fail(err);
+    driver.close();
+    driver.quit();
+    t.end();
+  });
+});
+
+test('Removing a server', function(t) {
+  var driver = seleniumHelpers.buildDriver();
+
+  driver.get('file://' + process.cwd() +
+      '/src/content/peerconnection/trickle-ice/index.html')
+  .then(function() {
+    t.pass('page loaded');
+    return driver.findElement(webdriver.By.css('#servers>option')).click();
+  })
+  .then(function() {
+    return driver.findElement(webdriver.By.id('remove')).click();
+  })
+  .then(function() {
+    return driver.findElement(webdriver.By.css('#servers'))
+        .getAttribute('length');
+  })
+  .then(function(length) {
+    t.ok(length === '0', 'server removed');
+    driver.close();
+    driver.quit();
+    t.end();
+  })
+  .then(null, function(err) {
+    t.fail(err);
+    driver.close();
+    driver.quit();
+    t.end();
+  });
+});
