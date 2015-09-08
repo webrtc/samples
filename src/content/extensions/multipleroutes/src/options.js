784d758f3e18eb785b49bbac45955cc1a4d07c56
Guo-wei Shieh <guoweis@google.com>
2015-09-08 15:24:27 -0700

    Create options page and support non-proxied UDP option

---
+++ src/content/extensions/multipleroutes/src/options.js
@@ -0,0 +1,41 @@
+// Saves options.
+function save_options() {
+  var multiroutes = document.getElementById('multiroutes').checked;
+  var nonproxiedudp = document.getElementById('nonproxiedudp').checked;
+  chrome.privacy.network.webRTCMultipleRoutesEnabled.set({'value': multiroutes});
+  try {
+    chrome.privacy.network.webRTCNonProxiedUdpEnabled.set({'value': nonproxiedudp});
+  }
+  catch(err) {
+    document.getElementById('nonproxiedudp').checked = false;
+    document.getElementById('nonproxiedudp').disabled = true;
+  }
+}
+
+// Restores checkbox states.
+function restore_options() {
+  chrome.privacy.network.webRTCMultipleRoutesEnabled.get({}, function(details) {
+    document.getElementById('multiroutes').checked = details.value;
+  });
+  try { 
+  chrome.privacy.network.webRTCNonProxiedUdpEnabled.get({}, function(details) {
+      document.getElementById('nonproxiedudp').checked = details.value;
+    });
+  }
+  catch(err){
+    document.getElementById('nonproxiedudp').checked = false;
+    document.getElementById('nonproxiedudp').disabled = true;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('multiroutes').addEventListener('click', save_options);
+document.getElementById('nonproxiedudp').addEventListener('click', save_options);
+
+document.title = chrome.i18n.getMessage('netli_options');
+var i18nElements = document.querySelectorAll('*[i18n-content]');
+for (var i = 0; i < i18nElements.length; i++) {
+  var elem = i18nElements[i];
+  var msg = elem.getAttribute('i18n-content');
+  elem.innerHTML = chrome.i18n.getMessage(msg);
+}
