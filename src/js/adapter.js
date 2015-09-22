d5893df856218fdd49efd790343fd565a51d4b5e
Philipp Hancke <fippo@andyet.net>
2015-09-22 12:13:01 -0700

    move trace from adapter to a common include file

--- src/js/adapter.js
+++ src/js/adapter.js
@@ -33,19 +33,6 @@ var webrtcUtils = {
   }
 };
 
-function trace(text) {
-  // This function is used for logging.
-  if (text[text.length - 1] === '\n') {
-    text = text.substring(0, text.length - 1);
-  }
-  if (window.performance) {
-    var now = (window.performance.now() / 1000).toFixed(3);
-    webrtcUtils.log(now + ': ' + text);
-  } else {
-    webrtcUtils.log(text);
-  }
-}
-
 if (typeof window === 'object') {
   if (window.HTMLMediaElement &&
     !('srcObject' in window.HTMLMediaElement.prototype)) {
---
+++ src/js/common.js
@@ -0,0 +1,12 @@
+function trace(text) {
+  // This function is used for logging.
+  if (text[text.length - 1] === '\n') {
+    text = text.substring(0, text.length - 1);
+  }
+  if (window.performance) {
+    var now = (window.performance.now() / 1000).toFixed(3);
+    console.log(now + ': ' + text);
+  } else {
+    console.log(text);
+  }
+}
