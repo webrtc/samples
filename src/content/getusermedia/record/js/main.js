2dfd9438e919daa4cdc96797b82472e5d96c21bd
Sam Dutton <samdutton@gmail.com>
2016-11-10 06:57:06 -0800

    Start Recording button disabled until gUM success

--- src/content/getusermedia/record/js/main.js
+++ src/content/getusermedia/record/js/main.js
@@ -51,6 +51,7 @@ var constraints = {
 };
 
 function handleSuccess(stream) {
+  recordButton.disabled =
   console.log('getUserMedia() got stream: ', stream);
   window.stream = stream;
   if (window.URL) {
