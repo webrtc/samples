1750d747f08619e4935bd3cc3266ab5440a1130d
Sam Dutton <samdutton@gmail.com>
2016-11-10 07:34:58 -0800

    Update main.js

--- src/content/getusermedia/record/js/main.js
+++ src/content/getusermedia/record/js/main.js
@@ -51,7 +51,7 @@ var constraints = {
 };
 
 function handleSuccess(stream) {
-  recordButton.disabled =
+  recordButton.disabled = false;
   console.log('getUserMedia() got stream: ', stream);
   window.stream = stream;
   if (window.URL) {
