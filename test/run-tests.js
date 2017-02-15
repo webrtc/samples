5f9ea1ce4938ecfc0945f1aa0f854970cf5afefb
Christoffer Jansson <jansson@google.com>
2017-02-15 09:19:24 +0100

    remove redundant comment

--- test/run-tests.js
+++ test/run-tests.js
@@ -22,9 +22,6 @@ require('../src/content/peerconnection/pc1/js/test');
 require('../src/content/peerconnection/audio/js/test');
 
 // Tests basic functionality of the peerconnection dtmf demo.
-// Disabled for Firefox due not supporting DTMF.
-// Disabled due to being flaky on Chrome.
-// TODO(jansson): Fix flakiness
 require('../src/content/peerconnection/dtmf/js/test');
 
 // Tests basic functionality of the peerconnection multiple demo.
