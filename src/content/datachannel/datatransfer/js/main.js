a3832525a7fb8ef05ac2dd8f50b7e1d6ec69d768
Vasanthakumar KK <vasanthakumar@google.com>
2018-03-26 16:24:01 +0200

    Added intentation and added a new variable for warning

--- src/content/datachannel/datatransfer/js/main.js
+++ src/content/datachannel/datatransfer/js/main.js
@@ -18,6 +18,7 @@ var orderedCheckbox = document.querySelector('input#ordered');
 var sendProgress = document.querySelector('progress#sendProgress');
 var receiveProgress = document.querySelector('progress#receiveProgress');
 var errorMessage = document.querySelector('div#errorMsg');
+var warningMessage = document.querySelector('div#warningMessage');
 
 var receivedSize = 0;
 var bytesToSend = 0;
@@ -30,8 +31,8 @@ megsToSend.addEventListener('change', function(e) {
     sendButton.disabled = true;
     errorMessage.innerHTML = '<p>Please enter a number greater than zero.</p>';
   } else if (this.value > 1000) {
-        errorMessage.innerHTML = '<p>Progress can be slow when the values are greater than 1000.</p>';
-        sendButton.disabled = false;
+    warningMessage.innerHTML = '<p>Progress can be slow when the values are greater than 1000.</p>';
+    sendButton.disabled = false;
   } else {
     errorMessage.innerHTML = '';
     sendButton.disabled = false;
