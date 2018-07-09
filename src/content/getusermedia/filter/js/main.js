b4efa391400d56c52a6b90a54070a2c93d73930e
Erik Hellman <erik.d.hellman@gmail.com>
2018-07-09 11:20:52 +0200

    ES6 refactoring of getusermedia samples

--- src/content/getusermedia/filter/js/main.js
+++ src/content/getusermedia/filter/js/main.js
@@ -8,12 +8,12 @@
 
 'use strict';
 
-var snapshotButton = document.querySelector('button#snapshot');
-var filterSelect = document.querySelector('select#filter');
+const snapshotButton = document.querySelector('button#snapshot');
+const filterSelect = document.querySelector('select#filter');
 
 // Put variables in global scope to make them available to the browser console.
-var video = window.video = document.querySelector('video');
-var canvas = window.canvas = document.querySelector('canvas');
+const video = window.video = document.querySelector('video');
+const canvas = window.canvas = document.querySelector('canvas');
 canvas.width = 480;
 canvas.height = 360;
 
@@ -26,7 +26,7 @@ filterSelect.onchange = function() {
   video.className = filterSelect.value;
 };
 
-var constraints = {
+const constraints = {
   audio: false,
   video: true
 };
@@ -40,5 +40,4 @@ function handleError(error) {
   console.log('navigator.getUserMedia error: ', error);
 }
 
-navigator.mediaDevices.getUserMedia(constraints).
-    then(handleSuccess).catch(handleError);
+navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
