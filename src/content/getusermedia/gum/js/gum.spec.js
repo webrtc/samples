db4c4971f2242ea5d72a8aff98d370288c337111
Erik Hellman <erik.d.hellman@gmail.com>
2018-07-11 14:34:22 +0200

    Initial UI test for basic gum sample using webdriver.io

---
+++ src/content/getusermedia/gum/js/gum.spec.js
@@ -0,0 +1,8 @@
+import GumPage from './gum.page'
+
+describe('getUserMedia basic', () => {
+  it('should stream camera to to video element', () => {
+    GumPage.open();
+    GumPage.waitForSuccessMessage();
+  });
+});
