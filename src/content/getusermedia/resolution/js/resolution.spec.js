05e16ebc285f8a6b89a39f5e4b6548e6e5ea874a
Erik Hellman <erik.d.hellman@gmail.com>
2018-07-16 14:21:09 +0200

    Reload page on each test

--- src/content/getusermedia/resolution/js/resolution.spec.js
+++ src/content/getusermedia/resolution/js/resolution.spec.js
@@ -2,8 +2,8 @@ import ResolutionPage from './resolution.page';
 import assert from 'assert';
 
 describe('getUserMedia resolution', () => {
-  ResolutionPage.open();
   it('should be 320 wide at qvga', () => {
+    ResolutionPage.open();
     ResolutionPage.qvgaButton.click();
     browser.waitForVisible('#gum-res-local');
     const videoLocal = browser.element('#gum-res-local');
@@ -12,6 +12,7 @@ describe('getUserMedia resolution', () => {
     assert.equal(320, width);
   });
   it('should be 640 wide at vga', () => {
+    ResolutionPage.open();
     ResolutionPage.vgaButton.click();
     browser.waitForVisible('#gum-res-local');
     const videoLocal = browser.element('#gum-res-local');
@@ -20,6 +21,7 @@ describe('getUserMedia resolution', () => {
     assert.equal(640, width);
   });
   it('should be 1280 wide at hd', () => {
+    ResolutionPage.open();
     ResolutionPage.hdButton.click();
     browser.waitForVisible('#gum-res-local');
     const videoLocal = browser.element('#gum-res-local');
