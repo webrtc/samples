ef98fd671b594c7f85655cfa0281346d0dcfe01c
Erik Hellman <erik.d.hellman@gmail.com>
2018-08-03 15:01:41 +0200

    Added missing file

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'extends': 'google',
+  'parserOptions': {
+    'ecmaVersion': 2017,
+  },
+  'rules': {
+    'max-len': 'off',
+    'require-jsdoc': 'off',
+    'arrow-parens': 'off',
+    'comma-dangle': 'off',
+    'no-throw-literal': 'off',
+    'camelcase': 'off',
+    'prefer-rest-params': 'off',
+    'no-invalid-this': 'off',
+    'eol-last': 'off',
+  },
+};
