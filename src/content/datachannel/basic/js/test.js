9ee59c0cead6d0a7afa108d6f01210e91f44ea55
Erik Hellman <erik.d.hellman@gmail.com>
2018-08-13 16:53:50 +0200

    Additional datachannel tests

--- src/content/datachannel/basic/js/test.js
+++ src/content/datachannel/basic/js/test.js
@@ -1,49 +1,31 @@
 /*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
  *  tree.
  */
-/* eslint-env node */
-
-'use strict';
-
-// This is a basic test file for use with testling.
-// The test script language comes from tape.
-const test = require('tape');
-
-// Disabling on firefox until sendKeys is fixed.
-// https://github.com/mozilla/geckodriver/issues/683
-test('Basic datachannel sample', {skip: process.env.BROWSER === 'firefox'},
-  t => {
-    const webdriver = require('selenium-webdriver');
-    const seleniumHelpers = require('webrtc-utilities').seleniumLib;
-    const driver = seleniumHelpers.buildDriver();
+export default {
+  'It should transfer text over data channel': (browser) => {
     const path = '/src/content/datachannel/basic/index.html';
-    const url = (process.env.BASEURL ? process.env.BASEURL : ('file://' + process.cwd())) + path;
+    const url = 'file://' + process.cwd() + path;
 
-    driver.get(url)
-      .then(() => {
-        t.pass('page loaded');
-        return driver.findElement(webdriver.By.id('startButton')).click();
-      })
-      .then(() => driver.wait(() => driver.executeScript(
-        'return remoteConnection && ' +
-        'remoteConnection.iceConnectionState === \'connected\';')))
-      .then(() => {
-        t.pass('remoteConnection ICE connected');
-        return driver.findElement(webdriver.By.id('dataChannelSend')).sendKeys('hello world');
-      })
-      .then(() => driver.findElement(webdriver.By.id('sendButton')).click())
-      .then(() => driver.wait(() => {
-        return driver.executeScript('return document.getElementById(\'dataChannelReceive\').value !== \'\'');
-      }))
-      .then(() => driver.findElement(webdriver.By.id('dataChannelReceive')).getAttribute('value'))
-      .then(value => t.ok(value === 'hello world', 'Text was received'))
-      .then(() => t.end())
-      .then(null, err => {
-        t.fail(err);
-        t.end();
-      });
-  });
+    browser
+      .url(url)
+      .click('#startButton')
+      .expect.element('#sendButton').to.be.enabled.before(50);
+    browser.expect.element('#dataChannelSend').to.be.enabled.before(50);
+
+    browser.setValue('#dataChannelSend', 'HELLO, WORLD!');
+    browser
+      .click('#sendButton')
+      .pause(50)
+      .assert.value('#dataChannelReceive', 'HELLO, WORLD!');
+
+    browser
+      .click('#closeButton')
+      .expect.element('#sendButton').to.not.be.enabled.before(50);
+
+    browser.end();
+  }
+};
