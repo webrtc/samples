eec518f4590a7e9c840a3d9da7904feb01af6a12
Harald Alvestrand <hta@chromium.org>
2019-06-06 13:30:04 +0200

    Review comments, lint

--- src/content/capture/canvas-filter/js/main.js
+++ src/content/capture/canvas-filter/js/main.js
@@ -1,5 +1,5 @@
 /*
-*  Copyright (c) 2016 The WebRTC project authors. All Rights Reserved.
+*  Copyright (c) 2019 The WebRTC project authors. All Rights Reserved.
 *
 *  Use of this source code is governed by a BSD-style license
 *  that can be found in the LICENSE file in the root of the source
@@ -11,13 +11,14 @@
 'use strict';
 
 const source = document.querySelector('#source');
+// TODO(hta): Use OffscreenCanvas for the intermediate canvases.
 const canvasIn = document.querySelector('#canvas-source');
 const canvasOut = document.querySelector('#canvas-result');
 const result = document.querySelector('#result');
 
 const stream = canvasOut.captureStream();
-var inputStream = null;
-var imageData = null;
+let inputStream = null;
+let imageData = null;
 
 result.srcObject = stream;
 
@@ -25,16 +26,17 @@ function loop() {
   if (source.videoWidth > 0 && source.videoHeight > 0) {
     canvasIn.width = source.videoWidth;
     canvasIn.height = source.videoHeight;
-    var ctx = canvasIn.getContext('2d');
+    let ctx = canvasIn.getContext('2d');
     ctx.drawImage(source, 0, 0);
-    ctx.fillStyle = "#FF0000";
+    // Put a red square into the image, to mark it as "processed".
+    ctx.fillStyle = '#FF0000';
     ctx.fillRect(10, 10, 80, 80);
     imageData = ctx.getImageData(0, 0, canvasIn.width, canvasIn.height);
     // At this point, we have data that can be transferred.
     // We paint it on the second canvas.
     canvasOut.width = source.videoWidth;
     canvasOut.height = source.videoHeight;
-    var outCtx = canvasOut.getContext('2d');
+    let outCtx = canvasOut.getContext('2d');
     outCtx.putImageData(imageData, 0, 0);
   }
   window.requestAnimationFrame(loop);
