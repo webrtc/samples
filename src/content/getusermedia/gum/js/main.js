acefa648d9bef87595bc81f79a41ea450d83f073
Q. Alex Zhao <qaz@chromium.org>
2020-01-09 15:22:03 -0800

    Add bare-minimum audio and device enumeration to the GUM sample.
    This make the GUM sample slightly more complete, and turns it into a
    useful test program for examining combined API behavior.

--- src/content/getusermedia/gum/js/main.js
+++ src/content/getusermedia/gum/js/main.js
@@ -8,28 +8,52 @@
 'use strict';
 
 // Put variables in global scope to make them available to the browser console.
-const constraints = window.constraints = {
+window.audioConstraints = {
+  audio: true,
+  video: false
+};
+window.videoConstraints = {
   audio: false,
   video: true
 };
 
-function handleSuccess(stream) {
-  const video = document.querySelector('video');
-  const videoTracks = stream.getVideoTracks();
-  console.log('Got stream with constraints:', constraints);
-  console.log(`Using video device: ${videoTracks[0].label}`);
-  window.stream = stream; // make variable available to browser console
-  video.srcObject = stream;
+function handleSuccess(constraints, stream) {
+  if (constraints.audio) {
+    console.log('Got audio stream with constraints:', constraints);
+
+    window.audioStream = stream;  // make variable available to browser console
+    const audioTracks = stream.getAudioTracks();
+    console.log(`Using audio device: ${audioTracks[0].label}`);
+
+    if (document.querySelector('#play-local-audio').checked) {
+      const audio = document.querySelector('audio');
+      audio.srcObject = stream;
+    }
+  }
+
+  if (constraints.video) {
+    console.log('Got video stream with constraints:', constraints);
+
+    window.videoStream = stream;  // make variable available to browser console
+    const videoTracks = stream.getVideoTracks();
+    console.log(`Using video device: ${videoTracks[0].label}`);
+
+    const video = document.querySelector('video');
+    video.srcObject = stream;
+  }
 }
 
-function handleError(error) {
-  if (error.name === 'ConstraintNotSatisfiedError') {
-    const v = constraints.video;
-    errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
-  } else if (error.name === 'PermissionDeniedError') {
-    errorMsg('Permissions have not been granted to use your camera and ' +
-      'microphone, you need to allow the page access to your devices in ' +
-      'order for the demo to work.');
+function handleError(constraints, error) {
+  if (error.name === 'ConstraintNotSatisfiedError' ||
+      error.name === 'OverconstrainedError') {
+    errorMsg(`The constraints ${
+        JSON.stringify(constraints)} are not supported by your device.`);
+  } else if (
+      error.name === 'NotAllowedError' ||
+      error.name === 'PermissionDeniedError') {
+    errorMsg(`Permissions have not been granted to use your ${
+        JSON.stringify(constraints)} devices. You need to allow the page access
+        to your devices in order for the demo to work.`);
   }
   errorMsg(`getUserMedia error: ${error.name}`, error);
 }
@@ -42,14 +66,35 @@ function errorMsg(msg, error) {
   }
 }
 
-async function init(e) {
+function toggleAudioPlayout() {
+  const audio = document.querySelector('audio');
+  const shouldPlay = document.querySelector('#play-local-audio').checked;
+  audio.srcObject =
+      shouldPlay && window.audioStream ? window.audioStream : null;
+}
+
+async function init(e, constraints) {
   try {
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    handleSuccess(stream);
+    handleSuccess(constraints, stream);
     e.target.disabled = true;
   } catch (e) {
-    handleError(e);
+    handleError(constraints, e);
   }
 }
 
-document.querySelector('#showVideo').addEventListener('click', e => init(e));
+document.querySelector('#play-local-audio')
+    .addEventListener('click', e => toggleAudioPlayout());
+document.querySelector('#showAudio')
+    .addEventListener('click', e => init(e, window.audioConstraints));
+document.querySelector('#showVideo')
+    .addEventListener('click', e => init(e, window.videoConstraints));
+document.querySelector('#enumerateDevices').addEventListener('click', e => {
+  navigator.mediaDevices.enumerateDevices().then(
+      (devices) => {
+        console.info('Enumerated devices:', devices);
+      },
+      (err) => {
+        console.error('Failed to enumerate devices:', err);
+      });
+});
