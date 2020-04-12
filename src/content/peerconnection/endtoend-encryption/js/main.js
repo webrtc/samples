e6b99c450f1c273fc27f71f8656c9180facd4191
Philipp Hancke <philipp.hancke@googlemail.com>
2020-04-12 17:05:27 +0200

    e2ee: improve dump to display more metadata (#1284)
    to simplify checking the metadata correctness.
    
    Co-authored-by: Harald Alvestrand <hta@google.com>

--- src/content/peerconnection/endtoend-encryption/js/main.js
+++ src/content/peerconnection/endtoend-encryption/js/main.js
@@ -109,7 +109,12 @@ function dump(chunk, direction, max = 16) {
   for (let j = 0; j < data.length && j < max; j++) {
     bytes += (data[j] < 16 ? '0' : '') + data[j].toString(16) + ' ';
   }
-  console.log(performance.now().toFixed(2), direction, bytes.trim(), chunk.data.byteLength, (data[0] & 0x1) === 0);
+  console.log(performance.now().toFixed(2), direction, bytes.trim(),
+      'len=' + chunk.data.byteLength,
+      'type=' + (chunk.type || 'audio'),
+      'ts=' + chunk.timestamp,
+      'ssrc=' + chunk.synchronizationSource
+  );
 }
 
 // If using crypto offset (controlled by a checkbox):
