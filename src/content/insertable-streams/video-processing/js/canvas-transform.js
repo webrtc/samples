807334e26d4fa0d8d2a95536a4f1117c127e7416
Ben Wagner <benjaminwagner@google.com>
2021-04-28 14:53:57 +0000

    git squash commit for vfdirect.
    65c8cb949f91891db82997528f58dd801531b409
    Use new canvas/WebGL APIs to draw VideoFrame directly and create VideoFrame from canvas
    
    52d197fd72c65b54408a16d00be7acdd611737f9
    eslint fix
    
    a75ae34fb3bcfce538528ed0232c24831c7081a5
    use single switch for ImageBitmap
    
    c0e4d50e144dcf351b5e16c4f949c18b23815074
    eslint
    
    78a117977e04ab013a8db1c79c84fd58aa0032e8
    Fix flipY and typo

--- src/content/insertable-streams/video-processing/js/canvas-transform.js
+++ src/content/insertable-streams/video-processing/js/canvas-transform.js
@@ -14,14 +14,27 @@
  */
 class CanvasTransform { // eslint-disable-line no-unused-vars
   constructor() {
-    // All fields are initialized in init()
-    /** @private {?OffscreenCanvas} canvas used to create the 2D context */
+    /**
+     * @private {?OffscreenCanvas} canvas used to create the 2D context.
+     *     Initialized in init.
+     */
     this.canvas_ = null;
     /**
      * @private {?CanvasRenderingContext2D} the 2D context used to draw the
-     *     effect
+     *     effect. Initialized in init.
      */
     this.ctx_ = null;
+    /**
+     * @private {boolean} If false, pass VideoFrame directly to
+     * CanvasRenderingContext2D.drawImage and create VideoFrame directly from
+     * this.canvas_. If either of these operations fail (it's not supported in
+     * Chrome <90 and broken in Chrome 90: https://crbug.com/1184128), we set
+     * this field to true; in that case we create an ImageBitmap from the
+     * VideoFrame and pass the ImageBitmap to drawImage on the input side and
+     * create the VideoFrame using an ImageBitmap of the canvas on the output
+     * side.
+     */
+    this.use_image_bitmap_ = false;
     /** @private {string} */
     this.debugPath_ = 'debug.pipeline.frameTransform_';
   }
@@ -51,16 +64,28 @@ class CanvasTransform { // eslint-disable-line no-unused-vars
     const height = frame.displayHeight;
     this.canvas_.width = width;
     this.canvas_.height = height;
-    // VideoFrame.timestamp is technically optional, but that should never
-    // happen here.
-    // TODO(benjaminwagner): Follow up if we should change the spec so this is
-    // non-optional.
-    const timestamp = /** @type {number} */ (frame.timestamp);
-    const inputBitmap = await frame.createImageBitmap();
-    frame.close();
+    const timestamp = frame.timestamp;
 
-    ctx.drawImage(inputBitmap, 0, 0);
-    inputBitmap.close();
+    if (!this.use_image_bitmap_) {
+      try {
+        // Supported for Chrome 90+.
+        ctx.drawImage(frame, 0, 0);
+      } catch (e) {
+        // This should only happen on Chrome <90.
+        console.log(
+            '[CanvasTransform] Failed to draw VideoFrame directly. Falling ' +
+                'back to ImageBitmap.',
+            e);
+        this.use_image_bitmap_ = true;
+      }
+    }
+    if (this.use_image_bitmap_) {
+      // Supported for Chrome <92.
+      const inputBitmap = await frame.createImageBitmap();
+      ctx.drawImage(inputBitmap, 0, 0);
+      inputBitmap.close();
+    }
+    frame.close();
 
     ctx.shadowColor = '#000';
     ctx.shadowBlur = 20;
@@ -68,10 +93,24 @@ class CanvasTransform { // eslint-disable-line no-unused-vars
     ctx.strokeStyle = '#000';
     ctx.strokeRect(0, 0, width, height);
 
-    const outputBitmap = await createImageBitmap(this.canvas_);
-    const outputFrame = new VideoFrame(outputBitmap, {timestamp});
-    outputBitmap.close();
-    controller.enqueue(outputFrame);
+    if (!this.use_image_bitmap_) {
+      try {
+        controller.enqueue(new VideoFrame(this.canvas_, {timestamp}));
+      } catch (e) {
+        // This should only happen on Chrome <91.
+        console.log(
+            '[CanvasTransform] Failed to create VideoFrame from ' +
+                'OffscreenCanvas directly. Falling back to ImageBitmap.',
+            e);
+        this.use_image_bitmap_ = true;
+      }
+    }
+    if (this.use_image_bitmap_) {
+      const outputBitmap = await createImageBitmap(this.canvas_);
+      const outputFrame = new VideoFrame(outputBitmap, {timestamp});
+      outputBitmap.close();
+      controller.enqueue(outputFrame);
+    }
   }
 
   /** @override */
