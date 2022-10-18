da595a058021b4bcecfa00e8b6a7a29db894f500
Philipp Hancke <philipp.hancke@googlemail.com>
2022-10-18 20:52:57 +0200

    fix js error and add linting for test

--- test/steps.js
+++ test/steps.js
@@ -8,37 +8,37 @@
 const TIMEOUT = 10000;
 
 function step(drivers, cb, logMessage) {
-    return Promise.all(drivers.map(driver => {
-        return cb(driver);
-    })).then(() => {
-        if (logMessage) {
-            console.log(logMessage);
-        }
-    });
+  return Promise.all(drivers.map(driver => {
+    return cb(driver);
+  })).then(() => {
+    if (logMessage) {
+      console.log(logMessage);
+    }
+  });
 }
 function waitNVideosExist(driver, n) {
-    return driver.wait(() => {
-        return driver.executeScript(n => document.querySelectorAll('video').length === n, n);
-    }, TIMEOUT);
+  return driver.wait(() => {
+    return driver.executeScript(n => document.querySelectorAll('video').length === n, n);
+  }, TIMEOUT);
 }
 
 function waitAllVideosHaveEnoughData(driver) {
-    return driver.wait(() => {
-        return driver.executeScript(() => {
-            const videos = document.querySelectorAll('video');
-            let ready = 0;
-            for (let i = 0; i < videos.length; i++) {
-                if (videos[i].readyState >= videos[i].HAVE_ENOUGH_DATA) {
-                    ready++;
-                }
-            }
-            return ready === videos.length;
-        });
-    }, TIMEOUT);
+  return driver.wait(() => {
+    return driver.executeScript(() => {
+      const videos = document.querySelectorAll('video');
+      let ready = 0;
+      for (let i = 0; i < videos.length; i++) {
+        if (videos[i].readyState >= videos[i].HAVE_ENOUGH_DATA) {
+          ready++;
+        }
+      }
+      return ready === videos.length;
+    });
+  }, TIMEOUT);
 }
 
 module.exports = {
-    step,
-    waitNVideosExist,
-    waitAllVideosHaveEnoughData,
+  step,
+  waitNVideosExist,
+  waitAllVideosHaveEnoughData,
 };
--- test/webdriver.js
+++ test/webdriver.js
@@ -13,90 +13,90 @@ const firefox = require('selenium-webdriver/firefox');
 const safari = require('selenium-webdriver/safari');
 
 if (os.platform() === 'win32') {
-    process.env.PATH += ';' + process.cwd() + '\\node_modules\\chromedriver\\lib\\chromedriver\\';
-    process.env.PATH += ';' + process.cwd() + '\\node_modules\\geckodriver';
+  process.env.PATH += ';' + process.cwd() + '\\node_modules\\chromedriver\\lib\\chromedriver\\';
+  process.env.PATH += ';' + process.cwd() + '\\node_modules\\geckodriver';
 } else {
-    process.env.PATH += ':node_modules/.bin';
+  process.env.PATH += ':node_modules/.bin';
 }
 
 function buildDriver(browser = process.env.BROWSER || 'chrome', options = {bver: process.env.BVER}) {
-    // Chrome options.
-    let chromeOptions = new chrome.Options()
-        .addArguments('allow-insecure-localhost')
-        .addArguments('use-fake-device-for-media-stream')
-        .addArguments('allow-file-access-from-files';
-    if (options.chromeFlags) {
-      options.chromeFlags.forEach((flag) => chromeOptions.addArguments(flag));
-    }
+  // Chrome options.
+  const chromeOptions = new chrome.Options()
+      .addArguments('allow-insecure-localhost')
+      .addArguments('use-fake-device-for-media-stream')
+      .addArguments('allow-file-access-from-files');
+  if (options.chromeFlags) {
+    options.chromeFlags.forEach((flag) => chromeOptions.addArguments(flag));
+  }
 
-    if (options.chromepath) {
-      chromeOptions.setChromeBinaryPath(options.chromepath);
-    } else if (os.platform() === 'linux' && options.version) {
-      chromeOptions.setChromeBinaryPath('browsers/bin/chrome-' + options.version);
-    }
+  if (options.chromepath) {
+    chromeOptions.setChromeBinaryPath(options.chromepath);
+  } else if (os.platform() === 'linux' && options.version) {
+    chromeOptions.setChromeBinaryPath('browsers/bin/chrome-' + options.version);
+  }
 
-    if (!options.devices || options.headless) {
-      // GUM doesn't work in headless mode so we need this. See
-      // https://bugs.chromium.org/p/chromium/issues/detail?id=776649
-      chromeOptions.addArguments('use-fake-ui-for-media-stream');
-    } else {
-      // see https://bugs.chromium.org/p/chromium/issues/detail?id=459532#c22
-      const domain = 'https://' + (options.devices.domain || 'localhost') + ':' + (options.devices.port || 443) + ',*';
-      const exceptions = {
-        media_stream_mic: {},
-        media_stream_camera: {},
-      };
+  if (!options.devices || options.headless) {
+    // GUM doesn't work in headless mode so we need this. See
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=776649
+    chromeOptions.addArguments('use-fake-ui-for-media-stream');
+  } else {
+    // see https://bugs.chromium.org/p/chromium/issues/detail?id=459532#c22
+    const domain = 'https://' + (options.devices.domain || 'localhost') + ':' + (options.devices.port || 443) + ',*';
+    const exceptions = {
+      media_stream_mic: {},
+      media_stream_camera: {},
+    };
 
-      exceptions.media_stream_mic[domain] = {
-        last_used: Date.now(),
-        setting: options.devices.audio ? 1 : 2 // 0: ask, 1: allow, 2: denied
-      };
-      exceptions.media_stream_camera[domain] = {
-        last_used: Date.now(),
-        setting: options.devices.video ? 1 : 2
-      };
+    exceptions.media_stream_mic[domain] = {
+      last_used: Date.now(),
+      setting: options.devices.audio ? 1 : 2 // 0: ask, 1: allow, 2: denied
+    };
+    exceptions.media_stream_camera[domain] = {
+      last_used: Date.now(),
+      setting: options.devices.video ? 1 : 2
+    };
 
-      chromeOptions.setUserPreferences({
-        profile: {
-          content_settings: {
-            exceptions: exceptions
-          }
+    chromeOptions.setUserPreferences({
+      profile: {
+        content_settings: {
+          exceptions: exceptions
         }
-      });
-    }
+      }
+    });
+  }
 
-    const safariOptions = new safari.Options();
-    safariOptions.setTechnologyPreview(options.bver === 'unstable');
+  const safariOptions = new safari.Options();
+  safariOptions.setTechnologyPreview(options.bver === 'unstable');
 
-    // Firefox options.
-    const firefoxOptions = new firefox.Options();
-    let firefoxPath = firefox.Channel.RELEASE;
-    if (options.firefoxpath) {
-        firefoxPath = options.firefoxpath;
-    } else if (os.platform() == 'linux' && options.version) {
-        firefoxPath = 'browsers/bin/firefox-' + options.version;
-    }
-    if (options.headless) {
-        firefoxOptions.addArguments('-headless');
-    }
-    firefoxOptions.setBinary(firefoxPath);
-    firefoxOptions.setPreference('media.navigator.streams.fake', true);
-    firefoxOptions.setPreference('media.navigator.permission.disabled', true);
+  // Firefox options.
+  const firefoxOptions = new firefox.Options();
+  let firefoxPath = firefox.Channel.RELEASE;
+  if (options.firefoxpath) {
+    firefoxPath = options.firefoxpath;
+  } else if (os.platform() == 'linux' && options.version) {
+    firefoxPath = 'browsers/bin/firefox-' + options.version;
+  }
+  if (options.headless) {
+    firefoxOptions.addArguments('-headless');
+  }
+  firefoxOptions.setBinary(firefoxPath);
+  firefoxOptions.setPreference('media.navigator.streams.fake', true);
+  firefoxOptions.setPreference('media.navigator.permission.disabled', true);
 
-    const driver = new webdriver.Builder()
-        .setChromeOptions(chromeOptions)
-        .setSafariOptions(safariOptions)
-        .setFirefoxOptions(firefoxOptions)
-        .forBrowser(browser)
-        .setChromeService(
+  const driver = new webdriver.Builder()
+      .setChromeOptions(chromeOptions)
+      .setSafariOptions(safariOptions)
+      .setFirefoxOptions(firefoxOptions)
+      .forBrowser(browser)
+      .setChromeService(
           new chrome.ServiceBuilder().addArguments('--disable-build-check')
-        );
+      );
 
-    if (browser === 'firefox') {
-      driver.getCapabilities().set('marionette', true);
-      driver.getCapabilities().set('acceptInsecureCerts', true);
-    }
-    return driver.build();
+  if (browser === 'firefox') {
+    driver.getCapabilities().set('marionette', true);
+    driver.getCapabilities().set('acceptInsecureCerts', true);
+  }
+  return driver.build();
 }
 
 module.exports = {
