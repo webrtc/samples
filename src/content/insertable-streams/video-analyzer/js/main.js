de91e5a01b19a6c1787d6b7bb4f54b86a1bbc41f
Philipp Hancke <phancke@meta.com>
2025-08-04 20:37:55 -0700

    e2ee: remove encodedInsertableStreams flag
    which is no longer required since early 2024:
      https://issues.chromium.org/issues/40943169#comment21

--- src/content/insertable-streams/video-analyzer/js/main.js
+++ src/content/insertable-streams/video-analyzer/js/main.js
@@ -104,9 +104,7 @@ async function call() {
   pc1 = new RTCPeerConnection();
   console.log('Created local peer connection object pc1');
   pc1.addEventListener('icecandidate', e => onIceCandidate(pc1, e));
-  pc2 = new RTCPeerConnection({
-    encodedInsertableStreams: true,
-  });
+  pc2 = new RTCPeerConnection();
   console.log('Created remote peer connection object pc2');
   pc2.addEventListener('icecandidate', e => onIceCandidate(pc2, e));
   pc1.addEventListener('iceconnectionstatechange', e => onIceStateChange(pc1, e));
